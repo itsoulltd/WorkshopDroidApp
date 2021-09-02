e98cfb190e117c9ed47d9d92e5d27fed54194e83
Towhidul Islam <m.towhid.islam@gmail.com>
2021-09-02 20:38:58 +0600

    RiderDetail added

---
+++ app/src/main/java/lab/infoworks/starter/ui/activities/riderDetail/RiderDetail.java
@@ -0,0 +1,14 @@
+package lab.infoworks.starter.ui.activities.riderDetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RiderDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(lab.infoworks.starter.R.layout.activity_rider_detail);
+    }
+}
