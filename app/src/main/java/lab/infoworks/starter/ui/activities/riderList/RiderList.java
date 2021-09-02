7ce2ae154b377d26759920c0e74f3e4caecb4bc8
Towhidul Islam <m.towhid.islam@gmail.com>
2021-09-02 20:32:47 +0600

    RiderList added

---
+++ app/src/main/java/lab/infoworks/starter/ui/activities/riderList/RiderList.java
@@ -0,0 +1,14 @@
+package lab.infoworks.starter.ui.activities.riderList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RiderList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(lab.infoworks.starter.R.layout.activity_rider_list);
+    }
+}
