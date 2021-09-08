cdd2431024653bd2176a7b6f46629029c97ff1b5
Towhidul Islam <m.towhid.islam@gmail.com>
2021-09-08 13:00:24 +0600

    Converting AppActivity to AppFragment

--- app/src/main/java/lab/infoworks/starter/ui/activities/app/AppActivity.java
+++ app/src/main/java/lab/infoworks/starter/ui/activities/app/AppActivity.java
@@ -1,58 +1,40 @@
 package lab.infoworks.starter.ui.activities.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import com.infoworks.lab.rest.models.Message;
+
+import lab.infoworks.libshared.notifications.NotificationCenter;
 import lab.infoworks.starter.R;
-import lab.infoworks.starter.ui.activities.riderList.RiderList;
 
 
 public class AppActivity extends AppCompatActivity {
 
     private static final String TAG = AppActivity.class.getName();
 
-    @BindView(R.id.verificationStatusTextView)
-    TextView verificationStatusTextView;
-
-    @BindView(R.id.verifyButton)
-    Button verifyButton;
-
-    private AppViewModel appViewModel;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_app);
-        ButterKnife.bind(this);
-        //We can initialize viewModel here:
-        /*appViewModel = new AppViewModel(getApplication());*/
-        //Following is also acceptable way of getting viewModel:
-        appViewModel = new ViewModelProvider(this).get(AppViewModel.class);
-        appViewModel.getUserStatusObservable().observe(this, verificationResult -> {
-            //
-            Log.d(TAG, "===> result: " + verificationResult.isVerified());
-            verificationStatusTextView.setText("Rider is verified.... :) ");
-        });
-
+        /**/
         //Setting Up ActionBar Title
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null){
             actionBar.setTitle(R.string.app_name);
         }
+
+        NotificationCenter.addObserver(this, AppFragment.MOVE_TO_RIDERS_FRAGMENT, (context, data) -> {
+            //TODO:
+            String payload = data.getStringExtra("payload");
+            Message msg = new Message().setPayload(payload);
+            moveToRiders(msg);
+        });
 
         Log.d(TAG + "-lifecycle", "onCreate");
     }
@@ -104,16 +86,12 @@ public class AppActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG + "-lifecycle", "onDestroy");
+        NotificationCenter.removeObserver(this, AppFragment.MOVE_TO_RIDERS_FRAGMENT);
     }
 
-    @OnClick(R.id.verifyButton)
-    public void verifyRider() {
-        appViewModel.verifyUser();
-    }
-
-    @OnClick(R.id.riderListButton)
-    public void findRiders(View view) {
-        startActivity(new Intent(this, RiderList.class));
+    public void moveToRiders(Message msg) {
+        //startActivity(new Intent(this, RiderList.class));
+        Log.d(TAG, "Click moveToRiders: " + msg.getPayload());
     }
 
 }
---
+++ app/src/main/java/lab/infoworks/starter/ui/activities/app/AppFragment.java
@@ -0,0 +1,111 @@
+package lab.infoworks.starter.ui.activities.app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.infoworks.lab.rest.models.Message;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import lab.infoworks.libshared.notifications.NotificationCenter;
+import lab.infoworks.starter.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AppFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AppFragment extends Fragment {
+
+    public static final String TAG = AppFragment.class.getSimpleName();
+    public static final String MOVE_TO_RIDERS_FRAGMENT = "MOVE_TO_RIDERS_FRAGMENT";
+
+    private Unbinder unbinder;
+    @BindView(R.id.statusTextView)
+    TextView statusTextView;
+
+    @BindView(R.id.statusButton)
+    Button statusButton;
+
+    private AppViewModel appViewModel;
+
+    private static final String ARG_TITLE = "title";
+    private String fragTitle = "Hello Fragments";
+
+    public AppFragment() {
+        // Required empty public constructor
+    }
+
+    public static AppFragment newInstance(String title) {
+        AppFragment fragment = new AppFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TITLE, title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            fragTitle = getArguments().getString(ARG_TITLE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_app, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        statusTextView.setText(fragTitle);
+        //We can initialize viewModel here:
+        //appViewModel = new AppViewModel(getActivity().getApplication());
+        //Following is also acceptable way of getting viewModel:
+        appViewModel = new ViewModelProvider(this).get(AppViewModel.class);
+        appViewModel.getUserStatusObservable()
+                .observe(getViewLifecycleOwner(), verificationResult -> {
+            //
+            Log.d(TAG, "===> result: " + verificationResult.isVerified());
+            statusTextView.setText("Rider is verified.... :) ");
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.statusButton)
+    public void verifyRider() {
+        //TODO:
+        Log.d(TAG, "verifyRider clicked");
+        appViewModel.verifyUser();
+    }
+
+    @OnClick(R.id.moveToRidersButton)
+    public void findRiders(View view) {
+        //TODO:
+        Log.d(TAG, "findRiders clicked");
+        Message msg = new Message().setPayload("This is message from AppFragment.");
+        NotificationCenter.postNotification(getContext(), MOVE_TO_RIDERS_FRAGMENT, msg.marshallingToMap(true));
+    }
+}
