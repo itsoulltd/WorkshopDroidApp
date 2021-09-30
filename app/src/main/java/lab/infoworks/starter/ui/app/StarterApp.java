da993ad3ccec5ea8037e558648f3a6cb61cb1f6d
Towhidul Islam <m.towhid.islam@gmail.com>
2021-09-30 22:59:46 +0600

    Device UUID added

--- app/src/main/java/lab/infoworks/starter/ui/app/StarterApp.java
+++ app/src/main/java/lab/infoworks/starter/ui/app/StarterApp.java
@@ -3,15 +3,26 @@ package lab.infoworks.starter.ui.app;
 import android.app.Application;
 import android.util.Log;
 
+import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 import lab.infoworks.starter.BuildConfig;
+import lab.infoworks.starter.util.DeviceUuid;
 
 public class StarterApp extends Application {
+
+    public static Executor executor = Executors.newFixedThreadPool((Runtime.getRuntime().availableProcessors() / 2) + 1);
 
     @Override
     public void onCreate() {
         super.onCreate();
         //Initializing GeoTracker
         Log.i("StarterApp", "API Gateway: " + BuildConfig.api_gateway);
+        //Generate Device UUID:
+        UUID uuid = new DeviceUuid(getApplicationContext()).getUuid();
+        Log.d("StarterApp", "Device UUID: " + uuid);
         //
     }
+
 }
