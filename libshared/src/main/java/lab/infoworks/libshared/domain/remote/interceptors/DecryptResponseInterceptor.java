124b243dfb8555065c453275f3058b9c44944379
Towhidul Islam <m.towhid.islam@gmail.com>
2021-10-09 01:12:28 +0600

    bugfix

--- libshared/src/main/java/lab/infoworks/libshared/domain/remote/interceptors/DecryptResponseInterceptor.java
+++ libshared/src/main/java/lab/infoworks/libshared/domain/remote/interceptors/DecryptResponseInterceptor.java
@@ -1,10 +1,6 @@
 package lab.infoworks.libshared.domain.remote.interceptors;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import com.infoworks.lab.rest.models.Message;
-import com.infoworks.lab.rest.models.Response;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -13,56 +9,37 @@ import lab.infoworks.libshared.domain.remote.interceptors.definition.DecryptInte
 import lab.infoworks.libshared.util.crypto.SecretKeyStore;
 import okhttp3.MediaType;
 import okhttp3.ResponseBody;
-import okio.Buffer;
-import okio.BufferedSource;
 
 public class DecryptResponseInterceptor implements DecryptInterceptor {
 
     public ResponseBody decrypt(ResponseBody originalBody) {
 
-        return new ResponseBody() {
+        MediaType contentType = originalBody.contentType();
+        ResponseBody body = originalBody;
 
-            @Nullable @Override
-            public MediaType contentType() {
-                return originalBody.contentType();
-            }
-
-            @Override
-            public long contentLength() {
-                return -1l;
-            }
-
-            @NonNull @Override
-            public BufferedSource source() {
-                String subtype = originalBody.contentType().subtype();
-                if (subtype.contains("text")){
-                    String strRes = bodyToString(originalBody);
+        String subtype = originalBody.contentType().subtype();
+        if (subtype.contains("text")){
+            String strRes = bodyToString(originalBody);
+            String decrypted = SecretKeyStore.getInstance()
+                    .decrypt("towhid@gmail.com", strRes);
+            //
+            body = ResponseBody.create(decrypted.getBytes(StandardCharsets.UTF_8), contentType);
+        } else if(subtype.contains("json")) {
+            String json = bodyToString(originalBody);
+            if (Message.isValidJson(json)){
+                try {
+                    Message msg = Message.unmarshal(com.infoworks.lab.rest.models.Response.class, json);
                     String decrypted = SecretKeyStore.getInstance()
-                            .decrypt("towhid@gmail.com", strRes);
+                            .decrypt("towhid@gmail.com", msg.getPayload());
+                    msg.setPayload(decrypted);
                     //
-                    Buffer buffer = new Buffer();
-                    buffer.write(decrypted.getBytes(StandardCharsets.UTF_8));
-                    return buffer;
-                } else if(subtype.contains("json")) {
-                    String json = bodyToString(originalBody);
-                    if (Message.isValidJson(json)){
-                        try {
-                            Message msg = Message.unmarshal(Response.class, json);
-                            String decrypted = SecretKeyStore.getInstance()
-                                    .decrypt("towhid@gmail.com", msg.getPayload());
-                            msg.setPayload(decrypted);
-                            //
-                            Buffer buffer = new Buffer();
-                            buffer.write(msg.toString().getBytes(StandardCharsets.UTF_8));
-                            return buffer;
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    body = ResponseBody.create(msg.toString().getBytes(StandardCharsets.UTF_8), contentType);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                //if failed any:
-                return originalBody.source();
             }
-        };
+        }
+        //
+        return body;
     }
 }
