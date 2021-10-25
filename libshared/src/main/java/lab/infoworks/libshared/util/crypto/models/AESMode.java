be394e88e491ecc9e3a2ccef53544ee8bb33a2fd
Towhidul Islam <m.towhid.islam@gmail.com>
2021-10-25 11:54:02 +0600

    Adding SecretKeyStore Unit Testing

--- libshared/src/main/java/lab/infoworks/libshared/util/crypto/models/AESMode.java
+++ libshared/src/main/java/lab/infoworks/libshared/util/crypto/models/AESMode.java
@@ -4,7 +4,8 @@ public enum AESMode {
 
     AES_GCM_NoPadding("AES/GCM/NoPadding"),
     AES_ECB_PKCS5Padding("AES/ECB/PKCS5Padding"),
-    AES_CBC_PKCS7Padding("AES/CBC/PKCS7Padding");
+    AES_CBC_PKCS7Padding("AES/CBC/PKCS7Padding"),
+    RSA_ECB_PKCS1Padding("RSA/ECB/PKCS1Padding");
 
     private String val;
 
